6ddba9776b77b4d9a3138d9bbd5626593177bf37
GT2107 <tanmaygangurde6@gmail.com>
2025-08-08 13:21:27 +0530

    added reversearray

--- recursion/palindrom.java
+++ recursion/palindrom.java
@@ -22,3 +22,18 @@ public class palindrom {
 
  }   
 }
+
+/*
+ * this code will first take input from the user for the string
+ * convert to a lower case so that there is compatibility for the word even in camel case
+ * later it will be parsed into a character array
+ * using the stringvariable.toCharArray() method
+ * 
+ * and then we check if the current element is not to the corresponding element from the back of the array
+ * if yes then we just return false and then the answer will be not plaindrome
+ * 
+ * where as when the i reaches the middle word of the sentence then we will just return true as there can nothing be new later on to check 
+ * this saves time
+ * 
+ * this code helped me understand recursion in bettter manner
+ */
